b9ce6c46df80e46c4a5d2ef4d4c8dd563d6610d9
Sugunstar <venkatasugunadithya@gmail.com>
2026-08-13 20:49:29 +0530

    resloving deployment issues5

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 app = Flask(__name__, template_folder=TEMPLATE_DIR)
