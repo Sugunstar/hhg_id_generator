37f32a585855292fc5b8e7f1655ec555237b24cf
Sugunstar <venkatasugunadithya@gmail.com>
2026-08-13 20:24:54 +0530

    resloving deployment issues

--- api/app.py
+++ api/app.py
@@ -1,5 +1,10 @@
+import os
 from flask import Flask, render_template
-app = Flask(__name__)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+app = Flask(__name__, template_folder=TEMPLATE_DIR)
 
 @app.route('/') # type: ignore
 def home():
